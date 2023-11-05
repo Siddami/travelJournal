c8846770d60c50177f823748e414138f3dd23492
siddami <dharmmy621@gmail.com>
2023-11-05 11:51:42 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import '../index.css'
+
+export default function Navbar(){
+    return(
+        <header>
+            <nav className='navbar'>
+                <img src="/logo.svg" alt="world icon" />
+                <p>My Travel Journal</p>
+            </nav>
+        </header>
+    )
+}
